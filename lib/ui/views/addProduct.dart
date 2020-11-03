be1dc74bc18f6e09866866717c97f4cc396577e9
hexaludira <hexaludira@gmail.com>
2020-11-03 21:32:17 +0700

    lengkapi AddProduct

--- lib/ui/views/addProduct.dart
+++ lib/ui/views/addProduct.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_firestore/core/models/productModel.dart';
 import 'package:provider/provider.dart';
 import '../../core/viewmodels/CRUDModel.dart';
+import '../../core/viewmodels/CRUDModel.dart';
 
 class AddProduct extends StatefulWidget {
   @override
@@ -16,8 +17,42 @@ class _AddProductState extends State<AddProduct> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    var productProvider = Provider.of<CRUDModel>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Add Product'),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(12),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Product Title',
+                    fillColor: Colors.grey[300],
+                    filled: true,
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter Product Title';
+                    }
+                  },
+                  onSaved: (value) => title = value
+              ),
+              SizedBox(height: 16,),
+              TextFormField(
+                keyboardType: TextInputType.numberWithOptions(),
+                
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
