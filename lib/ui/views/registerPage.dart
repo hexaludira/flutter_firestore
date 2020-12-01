93a231192b9ac34d7325ee1bc143b87b4126b874
hexaludira <hexaludira@gmail.com>
2020-12-01 13:03:31 +0700

    Complete Reg_page

--- lib/ui/views/registerPage.dart
+++ lib/ui/views/registerPage.dart
@@ -129,14 +129,169 @@ class _RegisterPageState extends State<RegisterPage> {
                               }
                             },
                             decoration: InputDecoration(
-                              focusedBorder: 
+                              hintText: 'Password',
+                              fillColor: Colors.white,
+                              filled: true,
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                ), 
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              focusedErrorBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              errorBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                            ),
+                          ),
+
+                          SizedBox(height: 16,),
+                          Text(
+                            'Confirm Password',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(height: 8),
+                          TextFormField(
+                            controller: _confirmPassController,
+                            validator: (val) {
+                              if (val.isEmpty) {
+                                return 'Confirm Password tidak boleh kosong';
+                              } else if (val != _passController.text) {
+                                return 'Password dan confirm password harus sama';
+                              } else {
+                                return null;
+                              }
+                            },
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: 'Confirm Password',
+                              fillColor: Colors.white,
+                              filled: true,
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              focusedErrorBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              errorBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                            ),
+                          ),
+
+                          SizedBox(height: 20),
+                          Container(
+                            width: double.infinity,
+                            child: RaisedButton(
+                              onPressed: () async {
+                                if (_formKey.currentState.validate()) {
+                                  SignInSignUpResult result = await AuthService.createUser(
+                                    email: _emailController.text, pass: _passController.text
+                                  );
+                                  if (result.user != null) {
+                                    //Go to profile page/ firebase
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => ProfilePage(
+                                          user: result.user,
+                                        ))
+                                    );
+                                  } else {
+                                    //show dialog
+                                    showDialog(
+                                      context: context,
+                                      builder: (context) => AlertDialog(
+                                        title: Text('Error'),
+                                        content: Text(result.message),
+                                        actions: <Widget>[
+                                          FlatButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: Text('OK'),
+                                          ),
+                                        ],
+                                      )
+                                    );
+                                  }
+                                }
+                              },
+                              child: Text(
+                                'Register', style: TextStyle(color: Colors.white),
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              color: Color(0xFF4f4f4f),
+                              elevation: 0,
+                              padding: EdgeInsets.symmetric(vertical: 16),
                             ),
                           ),
                         ],
                       ),
                     ),
+                    SizedBox(height: 30),
                   ]),
-                )
+                ),
+                SliverFillRemaining(
+                  hasScrollBody: false,
+                  child: Container(
+                    alignment: Alignment.bottomCenter,
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          'Already have account?', style: TextStyle(color: Colors.white),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushAndRemoveUntil(
+                              context, 
+                              MaterialPageRoute(builder: (context) => LoginPage()),
+                              (route) => false
+                            );
+                          },
+                          child: Text(
+                            'Login Here', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
