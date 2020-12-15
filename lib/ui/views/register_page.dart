9e2c1f75f179b7116c61cadfd94e4117c8343776
hexaludira <hexaludira@gmail.com>
2020-12-15 19:16:54 +0700

    Clean Up

--- lib/ui/views/register_page.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_firestore/core/services/auth_service.dart';
-import 'package:flutter_firestore/ui/views/loginPage.dart';
-import 'package:flutter_firestore/ui/';
